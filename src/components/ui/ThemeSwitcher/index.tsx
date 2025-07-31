42e974d70d08e3e44b54fddffd225e4987ae4aae
leem28 <lemanhddt@gmail.com>
2025-07-31 14:04:34 +0700

    Feat : add theme toggle

---
+++ src/components/ui/ThemeSwitcher/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "../DropdownMenu";
+import { Button } from "../Button";
+import { Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+
+const ThemeSwitcher = () => {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon">
+          <Sun />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuRadioGroup value={theme} onValueChange={setTheme}>
+          <DropdownMenuRadioItem value="light">Light</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="dark">Dark</DropdownMenuRadioItem>
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default ThemeSwitcher;
